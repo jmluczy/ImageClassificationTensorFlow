f21742c4a8080fe180158b6cb47920529c5c999f
jmluczy <jmluczy@gmail.com>
2018-10-13 18:50:08 -0400

    add functions to see number of classes and class counts

--- Python/procutil.py
+++ Python/procutil.py
@@ -26,6 +26,49 @@ def copy_files(to_path, from_path=BASE_PATH):
         else:
             raise exc
             
+def _build_count_dict(folder_path):
+    subfolders = os.listdir(folder_path)
+    d = dict()
+    for folder in subfolders:
+        path = folder_path + '\\' + folder
+        im_count = len(os.listdir(path))
+        d[folder] = im_count
+    return d
+
+def merge_dicts(d1, d2):
+    """
+    Returns single dictionary merged from the two provided
+    if d1, d2 have a key in common, the values are added
+    Args:
+        d1 (dict): values are int
+        d2 (dict): values are int
+    """
+    d1_keys = d1.keys()
+    for k, v in d2.items(): #iterate through d2
+        if k in d1_keys:    #if key is in d1
+            d1[k] += d2[k]  # add them
+        else:
+            d1[k] = d2[k]
+            
+    return d1
+
+def get_dict_lab_field_counts(directory=BASE_PATH):
+    field = BASE_PATH + '\\field'
+    lab = BASE_PATH + '\\lab'
+    d_lab = _build_count_dict(BASE_PATH + '\\lab')
+    d_field = _build_count_dict(BASE_PATH + '\\field')
+    return merge_dicts(d_lab, d_field)
+
+def print_classes_and_counts(directory=BASE_PATH):
+    d = get_dict_lab_field()
+    print('count', '\t\t', 'genus_species')
+    print('---------------------------------------')
+    for k, v in sorted(d.items()):
+        if v < 100:
+            print(str(v) + ' ', '\t\t', k)
+        else:
+            print(v, '\t\t', k)
+            
 def list_image_paths(base_dir=BASE_PATH):
     """ Return list of full paths of all files within the base directory.
         Does this by iterating through each item (folders and files) in current directory
@@ -94,6 +137,5 @@ if __name__ == '__main__':
     ind_max_rat = [i for i in range(len(dims)) if dims[i][0]/dims[i][1] == max_ratio or dims[i][1]/dims[i][0] == max_ratio]
     im = im_list[ind_max_rat[0]]
     show_image(im)
-    
-    
+        
 
